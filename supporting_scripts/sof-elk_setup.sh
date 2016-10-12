83c0df3a4aeff3406b0bfb297c74c206088ae6bd
benrau87 <benrau87@users.noreply.github.com>
2016-10-12 11:48:22 -0500

    Update sof-elk_setup.sh

--- supporting_scripts/sof-elk_setup.sh
+++ supporting_scripts/sof-elk_setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+
+###Remove any ony config files to avoid numbering mixups
 rm /etc/logstash/conf.d/*
 
 cp ~/forensic-grr-elk/logstash_config/* /etc/logstash/conf.d/
 
-mkdir ~/forensic-grr-elk/dashboards
-mkdir ~/forensic-grr-elk/dashboards/dashboard
-mkdir ~/forensic-grr-elk/dashboards/search
-mkdir ~/forensic-grr-elk/dashboards/visualization
-mkdir ~/forensic-grr-elk/dashboards/index-pattern
