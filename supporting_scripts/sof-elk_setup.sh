893bb220de99a8599909ad6ec37f23edc9c66a35
benrau87 <benrau87@users.noreply.github.com>
2016-10-12 11:49:30 -0500

    Update sof-elk_setup.sh

--- supporting_scripts/sof-elk_setup.sh
+++ supporting_scripts/sof-elk_setup.sh
@@ -3,5 +3,15 @@
 ###Remove any ony config files to avoid numbering mixups
 rm /etc/logstash/conf.d/*
 
+mkdir usr/local/logstash-syslog/
+mkdir usr/local/logstash-passivedns/
+mkdir usr/local/logstash-netflow/
+mkdir usr/local/logstash-httpd/
+mkdir var/db/logstash/sincedb/
+
+mkdir /usr/local/sof-elk/grok-patterns/
+
+cp ~/forensic-grr-elk/grok-patterns/* /usr/local/sof-elk/grok-patterns/
+
 cp ~/forensic-grr-elk/logstash_config/* /etc/logstash/conf.d/
 
