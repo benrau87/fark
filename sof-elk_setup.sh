c2ab277cf21cd10c80615c030957870ab43ccda8
benrau87 <benrau87@users.noreply.github.com>
2016-10-12 09:49:21 -0500

    Update sof-elk_setup.sh

--- sof-elk_setup.sh
+++ sof-elk_setup.sh
@@ -2,3 +2,10 @@
 rm /etc/logstash/conf.d/*
 
 cp ~/forensic-grr-elk/logstash_config/* /etc/logstash/conf.d/
+
+mkdir ~/forensic-grr-elk/dashboards
+mkdir ~/forensic-grr-elk/dashboards/dashboard
+mkdir ~/forensic-grr-elk/dashboards/search
+mkdir ~/forensic-grr-elk/dashboards/visualization
+mkdir ~/forensic-grr-elk/dashboards/index-pattern
+
