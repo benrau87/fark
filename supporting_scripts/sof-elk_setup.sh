943c34daddcb9c342c9ce26680b000f5a1660e31
benrau87 <benrau87@users.noreply.github.com>
2016-10-13 08:48:18 -0500

    Create sof-elk_setup.sh

---
+++ supporting_scripts/sof-elk_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm /etc/logstash/conf.d/*
+
+mkdir /usr/local/logstash-syslog
+mkdir /usr/local/logstash-passivedns
+mkdir /usr/local/logstash-netflow
+mkdir /usr/local/logstash-httpd
+mkdir /var/db
+mkdir /var/db/logstash
+mkdir /var/db/logstash/sincedb
+mkdir/ usr/local/sof-elk
+mkdir/ usr/local/sof-elk/lib
+mkdir /usr/local/sof-elk/grok-patterns
+
+cp ~/forensic-grr-elk/grok-patterns/* /usr/local/sof-elk/grok-patterns/
+cp ~/forensic-grr-elk/lib/* usr/local/sof-elk/lib/
+cp ~/forensic-grr-elk/logstash_conf/* /etc/logstash/conf.d/
