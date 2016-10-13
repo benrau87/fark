2e2fb4ad8b60ae9a10e659024c08d5dd70a149c0
benrau87 <benrau87@users.noreply.github.com>
2016-10-13 15:26:10 -0500

    Update timesketch.sh

--- supporting_scripts/timesketch.sh
+++ supporting_scripts/timesketch.sh
@@ -5,4 +5,11 @@ echo
 echo "Installing PostgreSQL"
 apt-get -qq install postgresql python-psycopg2 -y
 
+echo "local   all             timesketch                              md5" | tee -a /etc/postgresql/9.3/main/pg_hba.conf
+service postgresql restart
+
+echo "Installing PIP"
+apt-get -qq python-pip python-dev libffi-dev -y
+
+pip install timesketch
 
