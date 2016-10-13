15bf835e5686969624bc56efbb22b4dfa859e59d
benrau87 <benrau87@users.noreply.github.com>
2016-10-13 16:01:10 -0500

    Update timesketch.sh

--- supporting_scripts/timesketch.sh
+++ supporting_scripts/timesketch.sh
@@ -4,12 +4,33 @@
 echo
 echo "Installing PostgreSQL"
 apt-get -qq install postgresql python-psycopg2 -y
+apt-get -qq install openjdk-7-jre-headless -y
 
-echo "local   all             timesketch                              md5" | tee -a /etc/postgresql/9.3/main/pg_hba.conf
+echo "local   all             timesketch                              md5" | tee -a /etc/postgresql/9.*/main/pg_hba.conf
 service postgresql restart
 
 echo "Installing PIP"
-apt-get -qq python-pip python-dev libffi-dev -y
+apt-get -qq install python-pip python-dev libffi-dev -y
 
 pip install timesketch
 
+touch /etc/timesketch.conf
+chmod 600 /etc/timesketch.conf
+
+openssl rand -base64 32 
+
+echo
+echo "What would you like your SQL username to be?"
+read name
+
+-u postgres createuser -d -P -R -S $name
+
+echo
+echo "Please retype the same password you just created"
+read pass
+echo
+echo "SQLALCHEMY_DATABASE_URI = u'postgresql://$name:$pass@localhost/timesketch'" | tee -a /etc/timesketch.conf
+
+tsctl add_user -u $name
+
+tsctl runserver -h 0.0.0.0 -p 5000
