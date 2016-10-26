25def9897077628f32833c603165a36cfc50478f
benrau87 <benrau87@users.noreply.github.com>
2016-10-26 11:13:36 -0500

    Update deb_packetbeat_shipper_install.sh

--- packetbeat/deb_packetbeat_shipper_install.sh
+++ packetbeat/deb_packetbeat_shipper_install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+###Installs packetbeat shipper on Linux_64-bit
+
+wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.0.0-amd64.deb
+
+dpkg -i packetbeat-*
+
+rm /etc/packetbeat/packetbeat.yml
+
+cp ~/forensic-grr-elk/packetbeat/Nix_packetbeat.yml /etc/packetbeat/packetbeat.yml
+
+service packetbeat restart
+
 
