6d0346549fa4a2d88d55dea3f6fa50345312cad2
benrau87 <benrau87@users.noreply.github.com>
2016-11-02 15:47:56 -0500

    Update 5.0_Upgrade.sh

--- supporting_scripts/5.0_Upgrade.sh
+++ supporting_scripts/5.0_Upgrade.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
 
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 service kibana stop
 service logstash stop
 service elasticsearch stop
 apt-get update && apt-get install kibana elasticsearch logstash -y
+
+echo
+echo "Restarting ELK stack...Please wait"
+echo
+#Use to restart the ELK stack services if needed
+service elasticsearch restart
+service logstash restart
+service kibana restart
+service nginx restart
