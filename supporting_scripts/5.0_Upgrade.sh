5782cf8c8a6f0e45dc7c6a4619853fe676545806
benrau87 <benrau87@users.noreply.github.com>
2016-11-02 12:33:55 -0900

    Update 5.0_Upgrade.sh

--- supporting_scripts/5.0_Upgrade.sh
+++ supporting_scripts/5.0_Upgrade.sh
@@ -1,20 +1,83 @@
 #!/bin/bash
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
 
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-service kibana stop
-service logstash stop
-service elasticsearch stop
-apt-get update && apt-get install kibana elasticsearch logstash -y
+dir=$PWD
 
+echo "Adding Repos"
 echo
-echo "Restarting ELK stack...Please wait"
 echo
-#Use to restart the ELK stack services if needed
-service elasticsearch restart
-service logstash restart
-service kibana restart
+
+add-apt-repository -y ppa:webupd8team/java
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+#echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
+#echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
+#echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
+#echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee -a /etc/apt/sources.list.d/logstash-2.2.x.list
+#echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
+#Wait for dpkg process to finish
+echo "Waiting for dpkg process to free up..."
+while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
+   sleep 1
+done
+echo
+echo "Updating APT and installing dependencies"
+
+apt-get -qq update
+apt-get -qq dist-upgrade -y
+apt-get -qq install ctags curl git vim vim-doc vim-scripts exfat-fuse exfat-utils zip python-virtualenv jq tshark -y
+apt-get -qq install oracle-java8-installer -y
+apt-get -qq install elasticsearch kibana nginx apache2-utils logstash -y
+
+#Wait for dpkg process to finish
+echo "Waiting for dpkg process to free up..."
+while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
+   sleep 1
+done
+####ELK Install###################################################################################################
+##################################################################################################################
+echo "Configuring ELK Stack"
+echo
+
+###Elastisearch
+echo "Configuring Elasticsearch"
+update-rc.d elasticsearch defaults 95 10
+systemctl enable elasticsearch.service
+service elasticsearch start
+sleep 2
+
+###Logstash
+echo "Configuring Logstash"
+update-rc.d logstash 95 10
+systemctl enable logstash.service
+service logstash start
+sleep 2
+
+###Kiabana
+echo "Configuring Kibana"
+echo "server.host: 127.0.0.1" | tee -a /opt/kibana/config/kibana.yml 
+update-rc.d kibana defaults 95 10
+systemctl enable kibana.service
+service kibana start
+sleep 2
+echo ------------------------------------------------
+echo ------------------------------------------------
+echo What would you like your Kibana username to be?
+read kibanauser
+htpasswd -c /etc/nginx/htpasswd.users $kibanauser
+ 
+#####Creates site default file
+mv /etc/nginx/sites-available/default /etc/nginx/
+cp $dir/lib/default /etc/nginx/sites-available/
 service nginx restart
+sleep 2
+
+
