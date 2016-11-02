e6ce09fa8b776efdb3f3cf4f302999b66a8952aa
benrau87 <benrau87@users.noreply.github.com>
2016-11-02 09:49:12 -0900

    Update sof-elk_setup.sh

--- supporting_scripts/sof-elk_setup.sh
+++ supporting_scripts/sof-elk_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm /etc/logstash/conf.d/*
-
 mkdir /usr/local/logstash-syslog
 mkdir /usr/local/logstash-passivedns
 mkdir /usr/local/logstash-netflow
@@ -18,4 +16,4 @@ cp -r ~/forensic-grr-elk/dashboards/* /usr/local/sof-elk/
 cp -r ~/forensic-grr-elk/sof_dashboards/* /usr/local/sof-elk/sof_dashboards/
 cp ~/forensic-grr-elk/grok-patterns/* /usr/local/sof-elk/grok-patterns/
 cp ~/forensic-grr-elk/lib/custom/* /usr/local/sof-elk/lib/
-cp ~/forensic-grr-elk/logstash_conf/* /etc/logstash/conf.d/
+
