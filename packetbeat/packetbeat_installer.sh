bd50d47faf19dc1990b33267e2384b6c821ff9b1
benrau87 <benrau87@users.noreply.github.com>
2016-11-02 09:44:46 -0900

    Update packetbeat_installer.sh

--- packetbeat/packetbeat_installer.sh
+++ packetbeat/packetbeat_installer.sh
@@ -17,6 +17,11 @@ cd beats/packetbeat/
 cp Nix_packetbeat.yml /etc/packetbeat/packetbeat.yml
 update-rc.d packetbeat defaults 95 10
 systemctl enable packetbeat.service
+
+echo "What is the IP or Hostname of your Logstash server?"
+read IP
+perl -pi -e s/localhost/$IP/g /etc/packetbeat/packetbeat.yml
+
 service packetbeat restart
 
 
