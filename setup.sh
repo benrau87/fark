071cf6a04af04ecde94a0121c3e71861b07dc186
benrau87 <benrau87@users.noreply.github.com>
2016-11-07 09:47:15 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,13 +22,14 @@ echo
 add-apt-repository -y ppa:webupd8team/java
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch |  apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+#echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 #echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 
 echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
-#echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+#echo "deb http://packages.elastic.co/kibana/4.6/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.6.x.list
 
 echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee -a /etc/apt/sources.list.d/logstash-2.2.x.list
-#echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+#echo 'deb http://packages.elastic.co/logstash/2.4/debian stable main' | sudo tee -a /etc/apt/sources.list.d/logstash-2.4.x.list
 
 #Wait for dpkg process to finish
 echo "Waiting for dpkg process to free up..."
