b34622de11bc2fb468630989c20c2d2a6dfddf26
benrau87 <benrau87@users.noreply.github.com>
2016-11-08 10:23:33 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -160,49 +160,10 @@ read -p "Do you want to install forensic tools? Y/N" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo ""
-    mkdir ~/Desktop/Cases 
-    mkdir ~/Desktop/Tools
-  
-# Add scripts from different sources
-    cd ~/Desktop/Tools/
-    apt-get -qq install sleuthkit autopsy -y
-    
-# https://www.virustotal.com/en/documentation/public-api/#getting-file-scans
-    wget -q -O ~/Desktop/Tools/vt.py \
-    https://raw.githubusercontent.com/Xen0ph0n/VirusTotal_API_Tool/master/vt.py && \
-    chmod +x ~/Desktop/Tools/vt.py 
-    echo "Installed vt.py"
-    
-# https://n0where.net/malware-analysis-damm/
-    git clone --quiet https://github.com/504ensicsLabs/DAMM 
-    echo "Installed DAMM"
-     
-# https://github.com/DidierStevens/DidierStevensSuite
-    git clone --quiet https://github.com/DidierStevens/DidierStevensSuite.git 
-    echo "Checked out DidierStevensSuite." 
-    
-# https://github.com/Yara-Rules/rules.git
-    git clone --quiet https://github.com/Yara-Rules/rules.git 
-    echo "Checked out Yara-Rules."
-
-# https://github.com/decalage2/oletools.git
-    git clone --quiet https://github.com/decalage2/oletools.git 
-    echo "Checked out oletools."
-    
-    git clone --quiet https://github.com/USArmyResearchLab/Dshell.git
-    echo "Checked out DShell."
-    
-     git clone --quie https://github.com/BinaryDefense/goatrider.git
-    echo "Checked out GoatRider."
-    
-    apt-get -qq install yara -y
-    
-    #Volatility   
-    git clone --quiet https://github.com/volatilityfoundation/volatility.git
-    echo "Dwonloaded Volatility"
-    cd volatility
-    python setup.py install
+    echo 
+    echo "Installing Forensic Toolkit...Please Wait"
+    echo
+    bash $dir/supporting_scripts/tools.sh
 fi
 ###Cleanup and add shortcuts
 cp $dir/sof_dashboards/optiv_logo.png /usr/local/
