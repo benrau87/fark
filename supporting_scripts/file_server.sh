df4b4f8fecfbd8dbbaab2af9df03195a31e8f85e
benrau87 <benrau87@users.noreply.github.com>
2016-11-10 09:34:05 -0600

    Update file_server.sh

--- supporting_scripts/file_server.sh
+++ supporting_scripts/file_server.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
 ###Starts a simple http server under the client install directory to serve up installation packages
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
 
+cd ~/Desktop
+mkdir $HOSTNAME.fileserver
+cd $HOSTNAME.fileserver
+
+echo
+echo "What is the IP or Hostname of the machine running the ELK Stack?"
+read host
+echo "Downloading beats shippers"
+
+git clone https://github.com/benrau87/beats.git
+wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.0.0-amd64.deb
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.0-amd64.deb
+wget https://download.elastic.co/beats/topbeat/topbeat_1.3.1_amd64.deb
+wget https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-5.0.0-windows-x86_64.zip
+
+echo 
+echo "Starting simple file server"
+echo 
+python -m SimpleHttpServer 8080
