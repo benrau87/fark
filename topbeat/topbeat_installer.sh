3696edc12974df17a3a5b9a75f52b26a909940c3
benrau87 <benrau87@users.noreply.github.com>
2017-03-01 07:42:56 -0600

    Update topbeat_installer.sh

--- topbeat/topbeat_installer.sh
+++ topbeat/topbeat_installer.sh
@@ -7,6 +7,9 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
+echo "What is the IP or Hostname of your Logstash server?"
+read IP
+
 wget https://download.elastic.co/beats/topbeat/topbeat_1.3.1_amd64.deb
 dpkg -i topbeat_*
 
@@ -18,8 +21,13 @@ cp topbeat.yml /etc/topbeat/topbeat.yml
 update-rc.d topbeat defaults 95 10
 systemctl enable topbeat.service
 
-echo "What is the IP or Hostname of your Logstash server?"
-read IP
 perl -pi -e s/localhost/$IP/g /etc/topbeat/topbeat.yml
 
+cd ..
+mkdir
+/usr/share/ca-certificates/logstash
+chmod 755 /usr/share/ca-certificates/logstash
+mv logstash-forwarder.crt /usr/share/ca-certificates/logstash/
+
+update-ca-certificates
 service topbeat restart
