c8800a01de35e39fb20d712d62bebb9bd8c61ac4
benrau87 <benrau87@users.noreply.github.com>
2017-03-01 07:44:23 -0600

    Update packetbeat_installer.sh

--- packetbeat/packetbeat_installer.sh
+++ packetbeat/packetbeat_installer.sh
@@ -6,6 +6,9 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
+echo "What is the IP or Hostname of your Logstash server?"
+read IP
+
 wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.0.0-amd64.deb
 apt-get -qq install libpcap0.8 -y
 dpkg -i packetbeat-*
@@ -18,9 +21,15 @@ cp Nix_packetbeat.yml /etc/packetbeat/packetbeat.yml
 update-rc.d packetbeat defaults 95 10
 systemctl enable packetbeat.service
 
-echo "What is the IP or Hostname of your Logstash server?"
-read IP
 perl -pi -e s/localhost/$IP/g /etc/packetbeat/packetbeat.yml
+
+cd ..
+mkdir
+/usr/share/ca-certificates/logstash
+chmod 755 /usr/share/ca-certificates/logstash
+mv logstash-forwarder.crt /usr/share/ca-certificates/logstash/
+
+update-ca-certificates
 
 service packetbeat restart
 
